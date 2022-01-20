f2c183b7f26573adb50836407d4662193897fa3d
SupaFront <bluewotka@gmail.com>
2022-01-20 14:45:08 +0200

    123

--- src/api/SlimmomAPI/SlimmomAPI.js
+++ src/api/SlimmomAPI/SlimmomAPI.js
@@ -13,6 +13,10 @@ class SlimmomAPI {
     this.axiosInstance.defaults.headers.common.Authorization = null;
   }
 
+  static getUserInfo() {
+    return this.axiosInstance.get("/user");
+  }
+
   static registerUser(data) {
     return this.axiosInstance.post("/auth/register", data);
   }
@@ -23,6 +27,31 @@ class SlimmomAPI {
 
   static logoutUser() {
     return this.axiosInstance.post("/auth/logout");
+  }
+
+  static getDailyRateInfo(request) {
+    return this.axiosInstance.post("/daily-rate", request);
+  }
+
+  static postGetUserDaily(id, request) {
+    return this.axiosInstance.post(`/daily-rate/${id}`, request);
+  }
+
+  static getProduct(search) {
+    this.axiosInstance.defaults.params = { search };
+    return this.axiosInstance.get("/product");
+  }
+
+  static postEatenProduct(product) {
+    return this.axiosInstance.post("/day", product);
+  }
+
+  static deleteEatenProduct(product) {
+    return this.axiosInstance.delete("/day", product);
+  }
+
+  static getDayInfo(date) {
+    return this.axiosInstance.post("/day/info", date);
   }
 }
 
---
+++ src/redux/userData/userDataOperations.js
@@ -0,0 +1,74 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import SlimmomAPI from "../../api/SlimmomAPI/SlimmomAPI";
+
+export const dailyRateInfo = createAsyncThunk(
+  "userData/dailyRateInfo",
+  async (request, { rejectWithValue }) => {
+    try {
+      const dailyInfo = await SlimmomAPI.getDailyRateInfo(request);
+      return dailyInfo;
+    } catch ({ message }) {
+      return rejectWithValue(message);
+    }
+  }
+);
+
+export const userDaily = createAsyncThunk(
+  "userData/userDaily",
+  async ({ id, request }, { rejectWithValue }) => {
+    try {
+      const dailyRate = await SlimmomAPI.postGetUserDaily(id, request);
+      return dailyRate;
+    } catch ({ message }) {
+      return rejectWithValue(message);
+    }
+  }
+);
+
+export const searchProduct = createAsyncThunk(
+  "userData/searchProduct",
+  async (search, { rejectWithValue }) => {
+    try {
+      const product = await SlimmomAPI.getProduct(search);
+      return product;
+    } catch ({ message }) {
+      return rejectWithValue(message);
+    }
+  }
+);
+
+export const addEatenProduct = createAsyncThunk(
+  "userData/addGetEatenProduct",
+  async (product, { rejectWithValue }) => {
+    try {
+      const response = await SlimmomAPI.postEatenProduct(product);
+      return response;
+    } catch ({ message }) {
+      return rejectWithValue(message);
+    }
+  }
+);
+
+export const removeEatenProduct = createAsyncThunk(
+  "userData/removeEatenProduct",
+  async (product, { rejectWithValue }) => {
+    try {
+      const response = await SlimmomAPI.deleteEatenProduct(product);
+      return response;
+    } catch ({ message }) {
+      return rejectWithValue(message);
+    }
+  }
+);
+
+export const dayInfo = createAsyncThunk(
+  "userData/getDayInfo",
+  async (date, { rejectWithValue }) => {
+    try {
+      const response = await SlimmomAPI.getDayInfo(date);
+      return response;
+    } catch ({ message }) {
+      return rejectWithValue(message);
+    }
+  }
+);
