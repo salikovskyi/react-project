e66f1908d6867f330b4bf0c601d31288f53d0d7d
Oleksandr-N <hekp1984@gmail.com>
2022-01-21 17:25:06 +0200

    init

--- src/components/FooterInfo/FooterInfo.jsx
+++ src/components/FooterInfo/FooterInfo.jsx
@@ -16,7 +16,7 @@ export default function FooterInfo() {
   console.log(str.replace(/^(\d+)-(\d+)-(\d+)$/, `$3.$2.$1`));
   return (
     <section className={styles.mainSection}>
-      <ContainerStyled width={535}>
+      {/* <ContainerStyled width={535}> */}
       <div className={styles.section}>
         <div className={styles.daySection}>
           <span className={styles.title}>Сводка за {date ? date : ""}</span>
@@ -48,7 +48,7 @@ export default function FooterInfo() {
           <p className={styles.productList}> {products.join(", ")}ggggggggg</p>
         </div>
       </div>
-      </ContainerStyled>
+      {/* </ContainerStyled> */}
     </section>
   );
 }
