1c62747af509abfdf6f7461e1c9aa69cb111ef4a
salikovskyi <osalikovsiy@gmail.com>
2022-01-21 12:39:01 +0300

    init

--- src/App.js
+++ src/App.js
@@ -4,19 +4,14 @@ import { useDispatch, useSelector } from "react-redux";
 import { refreshUser, registerUser } from "./redux/auth/authOperations";
 import { useEffect } from "react";
 import FooterInfo from "./components/FooterInfo";
+import CalculatorPage from "./pages/Calculator/Calculator";
 
-import CalorieForm from "./components/DailyRateForm/CalorieForm";
 function App() {
-<<<<<<< HEAD
   return (
     <div>
-      <h1>app</h1>
-      <CalorieForm />
+      <CalculatorPage/>
     </div>
   );
-=======
-  return <FooterInfo />;
->>>>>>> dev
 }
 
 export default App;
---
+++ src/pages/Calculator/Calculator.jsx
@@ -0,0 +1,14 @@
+import css from "./Calculator.module.css";
+import CalorieForm from "../../components/DailyRateForm/CalorieForm";
+import FooterInfo from "../../components/FooterInfo";
+import ContainerStyled from "../../components/_styled/Container.styled";
+export default function CalculatorPage() {
+  return (
+    <div className={css.calc_page}>
+      <ContainerStyled>
+        <CalorieForm />
+        <FooterInfo />
+      </ContainerStyled>
+    </div>
+  );
+}
