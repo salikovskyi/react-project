9c2fe1a28567c0cbe49dea0b673bb31e7d145d4b
salikovskyi <osalikovsiy@gmail.com>
2022-01-26 23:40:45 +0300

    all styles

--- src/pages/DiaryPage/DiaryPage.jsx
+++ src/pages/DiaryPage/DiaryPage.jsx
@@ -2,8 +2,19 @@ import styles from "./DiaryPage.module.css";
 import FooterInfo from "../../components/FooterInfo";
 import Dairy from "../../components/Diary";
 import ContainerStyled from "../../components/_styled/Container.styled";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { rootClass } from "../../redux/userData/userDataSlice";
 
 export default function Diary() {
+  
+  const dispatch = useDispatch();
+
+  useEffect(()=>{
+    dispatch(rootClass('SlimCalc'))},
+  [])
+
+
   return (
     <div className={styles.bg}>
       <ContainerStyled>
