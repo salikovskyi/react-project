ca45f8781c72deaf80cc03d2bc06dbc8a9f45a32
Aleksey Bobrovskyi <leshabobrovsky99@gmail.com>
2022-02-07 20:14:15 +0200

    add

--- src/components/RegistrationForm/RegistrationForm.jsx
+++ src/components/RegistrationForm/RegistrationForm.jsx
@@ -43,7 +43,7 @@ const RegistrationForm = () => {
       >
         <Form>
           <FormControl label="Имя *" type="text" name="username" />
-          <FormControl label="Почта *" name="email" type="email" />
+          <FormControl label="Логин *" name="email" type="email" />
           <FormControl label="Пароль *" type="password" name="password" />
           <div className={css.btn_container}>
             <Link to='/login'>
